5cfbeca5a039f446acef683b33d3579d06ebaece
Unknown <kendomorris@yahoo.com>
2017-10-11 07:45:15 -0600

    Edits on this assignment.

--- Assignments/FuntionsWithParameters_Assinment.cs
+++ Assignments/FuntionsWithParameters_Assinment.cs
@@ -9,6 +9,11 @@ public class Functions : MonoBehaviour {
 		DoStuff();
 		int result = AddNumbers(5,11);
 		print(result);
+		int total = Health(100,50);
+		print(total);
+		float sum = Money(1065.50f,650.00f);
+		print(sum);
+		Help();
 	}
 
 	public int AddNumbers(int num1,int numb2){
@@ -17,6 +22,25 @@ public class Functions : MonoBehaviour {
 
 		return result; 
 	}	
+	public int Health(int maxHealth,int damage){
+
+		int total = maxHealth - damage;
+		return total;
+	}
+	public float Money(float totalCash, float itemCostSword){
+
+		float sum = totalCash - itemCostSword;
+		return sum;
+
+	}
+	public void Help(){
+		print("HELP ME!!");
+
+	}
+	
+
+	
+
 
 	
 	
