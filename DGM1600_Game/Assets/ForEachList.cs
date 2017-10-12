355415af72c3b7e2e55bd5bb88b4f7af410e5390
Unknown <kendomorris@yahoo.com>
2017-10-11 20:21:30 -0600

    New scripts

--- DGM1600_Game/Assets/ForEachList.cs
+++ DGM1600_Game/Assets/ForEachList.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class ForEachList : MonoBehaviour {
 
+		
 	// Use this for initialization
 	void Start () {
+		List<FirstOrder> badguys = new List<FirstOrder>();
+
+		badguys.Add( new FirstOrder("Snoke",1000));
+		badguys.Add( new FirstOrder("General Hux",20));
+		badguys.Add( new FirstOrder("Kylo Ren",400));
+		badguys.Add( new FirstOrder("Captain Phasma",150));
+	
+		badguys.Sort();
+
+		foreach(FirstOrder guy in badguys){
+			print(guy.name +"  "+ guy.power);
+		}
 		
+
+
+
+
+		List<Champion> heros = new List<Champion>();
+
+		heros.Add( new Champion("Albon", 6000));
+		heros.Add( new Champion("Fay",1000));
+		heros.Add( new Champion("Alei",500));
+		
+
+	
 	}
+
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	
 }
--- DGM1600_Game/Assets/While.cs
+++ DGM1600_Game/Assets/While.cs
@@ -6,14 +6,34 @@ public class While : MonoBehaviour {
 
 
 	public int bottles = 0;
+	public int money = 0;
+	public int fuel = 0;
+	public int power = 200;
+	public float time = 100;
 
 
 	// Use this for initialization
 	void Start () {
-		while(bottles < 100){
-			bottles ++;
-			print(bottles + " bottles of beer on the wall!");
+		// while(bottles < 100){
+		// 	bottles ++;
+		// 	print(bottles + " bottles of beer on the wall!");
 			
+		// }
+		// while(money > 100){
+		// 	money --;
+		// 	print(money + "Coins!");
+		// }
+		// while(fuel < 100){
+		// 	fuel --;
+		// 	print(fuel + "Gas left!");
+		// }
+		// while(power < 200){
+		// 	power --;
+		// 	print("Your Magic is low!");
+		// }
+		while(time > 0){
+			time ++;
+			print("the time is" + time);
 		}
 
 	
